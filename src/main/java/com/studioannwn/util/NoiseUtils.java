660a31aa4273fe2391c0cfdc8f045855f212ab48
Soma Holiday <soma@somaholiday.com>
2020-02-04 16:31:16 -0800

    Add Noise pattern

---
+++ src/main/java/com/studioannwn/util/NoiseUtils.java
@@ -0,0 +1,20 @@
+package com.studioannwn.util;
+
+/**
+ * Perlin noise
+ */
+public class NoiseUtils {
+    private static NoiseHelper noiseHelper = new NoiseHelper();
+
+    public static float noise(float x) {
+        return noiseHelper.noise(x);
+    }
+
+    public static float noise(float x, float y) {
+        return noiseHelper.noise(x, y);
+    }
+
+    public static float noise(float x, float y, float z) {
+        return noiseHelper.noise(x, y, z);
+    }
+}
