b6a3a987453f972a9129af6ed30a926a78bbc2b2
Soma Holiday <soma@somaholiday.com>
2020-06-20 20:02:39 -0700

    Disable GL point cloud for Rasp Pi

--- src/main/java/com/studioannwn/Scale.java
+++ src/main/java/com/studioannwn/Scale.java
@@ -144,7 +144,7 @@ public class Scale extends PApplet {
     flags.showFramerate = false;
     flags.isP3LX = true;
     flags.immutableModel = true;
-    // flags.useGLPointCloud = false; // Uncomment to run on Raspberry Pi
+    flags.useGLPointCloud = false; // Uncomment to run on Raspberry Pi
 
     logger.info("Current renderer:" + sketchRenderer());
     logger.info("Current graphics:" + getGraphics());
