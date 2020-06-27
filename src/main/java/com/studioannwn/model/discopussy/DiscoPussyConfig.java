af65d001e70182f6452b4ea866e61a59f42ab2f3
Soma Holiday <soma@somaholiday.com>
2020-06-27 14:55:13 -0700

    Minor config polish

--- src/main/java/com/studioannwn/model/discopussy/DiscoPussyConfig.java
+++ src/main/java/com/studioannwn/model/discopussy/DiscoPussyConfig.java
@@ -2,6 +2,8 @@ package com.studioannwn.model.discopussy;
 
 import heronarts.lx.transform.LXVector;
 
+import static com.studioannwn.model.discopussy.TentacleConfig.Size.LONG;
+import static com.studioannwn.model.discopussy.TentacleConfig.Size.SHORT;
 import static com.studioannwn.util.DistanceConstants.INCHES;
 import static com.studioannwn.util.DistanceConstants.METER;
 
@@ -9,10 +11,16 @@ public class DiscoPussyConfig {
   public final static String PIXLITE_1 = "192.168.2.18";
   public final static String PIXLITE_2 = "192.168.2.19";
 
+  // position of disco ball
   public final static LXVector DISCO_BALL_CENTER = new LXVector(0, 0, 0);
+
+  // radius of disco ball
   public final static float DISCO_BALL_RADIUS = 24 * INCHES;
+
+  // space between disco ball and tentacle
   public final static float DISCO_BALL_TENTACLE_OFFSET = 6 * INCHES;
 
+  // distance of datalines from center of tentacle triangle
   public final static float DATALINE_DISTANCE = 1 * INCHES;
 
   public final static class LEDModule {
@@ -53,49 +61,53 @@ public class DiscoPussyConfig {
    */
 
   private final TentacleConfig[] tentacles = new TentacleConfig[]{
-    new TentacleConfig("0", 0, TentacleConfig.Size.LONG)
+
+    new TentacleConfig("0", 0, LONG)
       .setIpAddress(PIXLITE_1)
       .setBottomChannel(1)
       .setLeftChannel(2)
       .setRightChannel(3),
-    new TentacleConfig("1", 45, TentacleConfig.Size.SHORT)
+
+    new TentacleConfig("1", 45, SHORT)
       .setIpAddress(PIXLITE_1)
       .setBottomChannel(4)
       .setLeftChannel(5)
       .setRightChannel(6),
-    new TentacleConfig("2", 90, TentacleConfig.Size.LONG)
+
+    new TentacleConfig("2", 90, LONG)
       .setIpAddress(PIXLITE_1)
       .setBottomChannel(7)
       .setLeftChannel(8)
       .setRightChannel(9),
-    new TentacleConfig("3", 135, TentacleConfig.Size.SHORT)
+
+    new TentacleConfig("3", 135, SHORT)
       .setIpAddress(PIXLITE_1)
       .setBottomChannel(10)
       .setLeftChannel(11)
       .setRightChannel(12),
-    new TentacleConfig("4", 180, TentacleConfig.Size.LONG)
+
+    new TentacleConfig("4", 180, LONG)
       .setIpAddress(PIXLITE_2)
       .setBottomChannel(1)
       .setLeftChannel(2)
       .setRightChannel(3),
-    new TentacleConfig("5", 225, TentacleConfig.Size.SHORT)
+
+    new TentacleConfig("5", 225, SHORT)
       .setIpAddress(PIXLITE_2)
       .setBottomChannel(4)
       .setLeftChannel(5)
       .setRightChannel(6),
-    new TentacleConfig("6", 270, TentacleConfig.Size.LONG)
+
+    new TentacleConfig("6", 270, LONG)
       .setIpAddress(PIXLITE_2)
       .setBottomChannel(7)
       .setLeftChannel(8)
       .setRightChannel(9),
-    new TentacleConfig("7", 315, TentacleConfig.Size.SHORT)
+
+    new TentacleConfig("7", 315, SHORT)
       .setIpAddress(PIXLITE_2)
       .setBottomChannel(10)
       .setLeftChannel(11)
       .setRightChannel(12),
   };
-
-  public TentacleConfig[] getTentacleConfigs() {
-    return tentacles;
-  }
 }
