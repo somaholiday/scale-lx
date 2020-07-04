04cfc17327ad19219269f22178231e81097ce27b
Soma Holiday <soma@somaholiday.com>
2020-07-03 18:48:10 -0700

    Add Spiral pattern

---
+++ src/main/java/com/studioannwn/patterns/Spiral.java
@@ -0,0 +1,72 @@
+package com.studioannwn.patterns;
+
+import com.studioannwn.util.TimeConstants;
+import heronarts.lx.LX;
+import heronarts.lx.LXCategory;
+import heronarts.lx.color.LXColor;
+import heronarts.lx.model.LXPoint;
+import heronarts.lx.parameter.BooleanParameter;
+import heronarts.lx.parameter.CompoundParameter;
+import heronarts.lx.parameter.LXParameter;
+import heronarts.lx.pattern.LXPattern;
+
+import static com.studioannwn.util.MathConstants.TAU;
+import static com.studioannwn.util.MathUtils.*;
+
+@LXCategory(LXCategory.FORM)
+public class Spiral extends LXPattern {
+  CompoundParameter sizeParameter = new CompoundParameter("Size", 5, 1, 10).setDescription("Sets spiral width");
+  CompoundParameter spacingParameter = new CompoundParameter("Spacing", 0.5, 0.1, 1).setDescription("Sets spiral spacing");
+  CompoundParameter lengthParameter = new CompoundParameter("Length", 2, 1, 5).setDescription("Sets spiral length");
+  CompoundParameter speedParameter = new CompoundParameter("Speed", 10, -25, 25).setDescription("Sets rotation speed").setPolarity(LXParameter.Polarity.BIPOLAR);
+  CompoundParameter thetaIncParameter = new CompoundParameter("Curve", .1, -.1, .1).setDescription("Alters the spiral curvature").setPolarity(LXParameter.Polarity.BIPOLAR);
+
+  CompoundParameter hueParameter = new CompoundParameter("Hue", 0.5).setDescription("Sets hue of spiral");
+  BooleanParameter grayscaleParameter = new BooleanParameter("Gray", false).setDescription("Sets output to grayscale");
+
+  float t = 0;
+
+  public Spiral(LX lx) {
+    super(lx);
+
+    addParameter(sizeParameter.getLabel().toLowerCase(), sizeParameter);
+    addParameter(spacingParameter.getLabel().toLowerCase(), spacingParameter);
+    addParameter(lengthParameter.getLabel().toLowerCase(), lengthParameter);
+    addParameter(speedParameter.getLabel().toLowerCase(), speedParameter);
+    addParameter(thetaIncParameter.getLabel().toLowerCase(), thetaIncParameter);
+    addParameter(hueParameter.getLabel().toLowerCase(), hueParameter);
+    addParameter(grayscaleParameter.getLabel().toLowerCase(), grayscaleParameter);
+  }
+
+  @Override
+  protected void run(double deltaMs) {
+    t += (float) deltaMs / TimeConstants.SEC * speedParameter.getValuef() * -1;
+    t = t % TAU;
+    setColors(LXColor.BLACK);
+
+    float width = sizeParameter.getValuef();
+    float spacing = spacingParameter.getValuef();
+    float iterations = lengthParameter.getValuef() * 72;
+    float thetaIncrement = thetaIncParameter.getValuef();
+
+    float h = hueParameter.getValuef() * 360;
+    float s = grayscaleParameter.isOn() ? 0 : 100;
+    float b = 100;
+
+    float r = 0;
+    float theta = 0;
+    for (int i = 0; i < iterations; i++) {
+      float x = r * cos(theta + t);
+      float y = r * sin(theta + t);
+
+      for (LXPoint p : model.points) {
+        if (dist(x, y, p.x, p.y) < width) {
+          colors[p.index] = lx.hsb(h, s, b);
+        }
+
+      }
+      r += spacing;
+      theta += thetaIncrement;
+    }
+  }
+}
