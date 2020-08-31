f501ab042fdc05a07a257256055d1ef0f4b09321
Soma Holiday <soma@somaholiday.com>
2020-08-30 17:13:39 -0700

    Mapping adjustments

--- src/main/java/com/studioannwn/model/FixtureDef.java
+++ src/main/java/com/studioannwn/model/FixtureDef.java
@@ -14,10 +14,8 @@ public class FixtureDef {
   private int ledCount;
 
   public FixtureDef(float x, float y, float z, int ledCount) {
-    float factor = Math.abs(PApplet.map(z, 6, 64, -1, 1));
-    float yAdj = (float) (3 + 9 * factor);
-    this.position = new LXVector(x * DistanceConstants.IN, yAdj * DistanceConstants.IN, z * DistanceConstants.IN);
-    // this.position = new LXVector(x * DistanceConstants.IN, y * DistanceConstants.IN, z * DistanceConstants.IN);
+    // note: y is negative because measurements were taken from ceiling
+    this.position = new LXVector(x * DistanceConstants.IN, -y * DistanceConstants.IN, z * DistanceConstants.IN);
     this.ledCount = ledCount;
   }
 
