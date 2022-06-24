b83d469b5a0ffe561d40f721c98b5682dcc82078
Soma Holiday <soma@somaholiday.com>
2022-06-24 13:42:28 -0700

    GPIO listening is working

--- src/main/java/com/studioannwn/AnnwnLX.java
+++ src/main/java/com/studioannwn/AnnwnLX.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.lang.reflect.Modifier;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -11,6 +13,15 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 import com.google.common.reflect.ClassPath;
+
+import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.pi4j.platform.PlatformAlreadyAssignedException;
+import com.pi4j.system.SystemInfo;
+import com.pi4j.util.CommandArgumentParser;
+import com.pi4j.util.Console;
+import com.pi4j.util.ConsoleColor;
 
 import com.studioannwn.output.ladybug.LadybugLayout;
 import heronarts.lx.LX;
@@ -21,6 +32,8 @@ import heronarts.lx.studio.LXStudio;
 import processing.core.PApplet;
 
 public class AnnwnLX extends PApplet implements LXPlugin {
+  
+  public static GpioController gpio;
 
   // Configuration flags
   private final static boolean RESIZABLE = true;
@@ -29,7 +42,7 @@ public class AnnwnLX extends PApplet implements LXPlugin {
                                                       // Enable at your own risk!
                                                       // Could cause VM crashes.
 
-  public final static boolean DEBUG = true;
+  public final static boolean DEBUG = false;
 
   static {
     System.setProperty("java.util.logging.SimpleFormatter.format", "%3$s: %1$tc [%4$s] %5$s%6$s%n");
@@ -133,6 +146,8 @@ public class AnnwnLX extends PApplet implements LXPlugin {
     } catch (IOException ex) {
       logger.log(Level.SEVERE, "Error creating log file: " + LOG_FILENAME_PREFIX, ex);
     }
+    
+    setupButtons();
 
     LXStudio.Flags flags = new LXStudio.Flags(this);
     flags.showFramerate = false;
@@ -169,5 +184,93 @@ public class AnnwnLX extends PApplet implements LXPlugin {
 
   public void draw() {
     // All is handled by LX Studio
+  }
+  
+  private void setupButtons() {
+    // create Pi4J console wrapper/helper
+    // (This is a utility class to abstract some of the boilerplate code)
+    final Console console = new Console();
+
+    // print program title/header
+    console.title("<-- The Pi4J Project -->", "GPIO Listen (All Pins) Example");
+
+    // create GPIO controller
+    gpio = GpioFactory.getInstance();
+
+    // by default we will use gpio pin PULL-DOWN
+    PinPullResistance pull = PinPullResistance.PULL_DOWN;
+
+    // prompt user to wait
+    console.println(" ... please wait; provisioning GPIO pins with resistance [" + pull + "]");
+
+    // create pins collection to store provisioned pin instances
+    List<GpioPinDigitalInput> provisionedPins = new ArrayList<>();
+    Pin[] pins;
+
+    // get a collection of raw pins based on the board type (model)
+    SystemInfo.BoardType board;
+    try {
+       board = SystemInfo.getBoardType();
+    } catch (Exception ex) {
+       System.err.println(ex.getMessage());
+       return;
+    } 
+    
+    if(board == SystemInfo.BoardType.RaspberryPi_ComputeModule ||
+       board == SystemInfo.BoardType.RaspberryPi_ComputeModule3 ||
+       board == SystemInfo.BoardType.RaspberryPi_ComputeModule3_Plus) {
+        // get all pins for compute module 1 & 3 (CM4 uses 'RaspiPin' pin provider)
+        pins = RCMPin.allPins();
+    }
+    else {
+        // get exclusive set of pins based on RaspberryPi model (board type)
+        pins = RaspiPin.allPins(board);
+    }
+
+
+    // provision GPIO input pins with its internal pull resistor set
+    for (Pin pin : pins) {
+        try {
+            GpioPinDigitalInput provisionedPin = gpio.provisionDigitalInputPin(pin, pull);
+            provisionedPins.add(provisionedPin);
+
+            // un-export the provisioned GPIO pins when program exits
+            provisionedPin.setShutdownOptions(true);
+        }
+        catch (Exception ex){
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    // prompt user that we are ready
+    console.println(" ... GPIO pins provisioned and ready for use.");
+    console.emptyLine();
+
+    // --------------------------------
+    // EVENT-BASED GPIO PIN MONITORING
+    // --------------------------------
+
+    // create and register gpio pin listeners for event pins
+    gpio.addListener(new GpioPinListenerDigital() {
+        @Override
+        public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+            // display pin state on console
+            console.println(" --> GPIO PIN STATE CHANGE (EVENT): " + event.getPin() + " = " +
+                    ConsoleColor.conditional(
+                            event.getState().isHigh(), // conditional expression
+                            ConsoleColor.GREEN,        // positive conditional color
+                            ConsoleColor.RED,          // negative conditional color
+                            event.getState()));        // text to display
+        }
+    }, provisionedPins.toArray(new GpioPinDigitalInput[0]));
+    
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      public void run() {
+        console.println("SHUTTING DOWN GPIO");
+        // stop all GPIO activity/threads by shutting down the GPIO controller
+        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+        gpio.shutdown();
+      }
+    });
   }
 }
