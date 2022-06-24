c9887c174a775430f8923283c030f3ba89a0c83e
Soma Holiday <soma@somaholiday.com>
2022-06-24 11:35:36 -0700

    Add Ripple switch to make wavelength tiny

--- src/main/java/com/studioannwn/patterns/Ripple.java
+++ src/main/java/com/studioannwn/patterns/Ripple.java
@@ -8,6 +8,7 @@ import heronarts.lx.model.LXPoint;
 import heronarts.lx.modulator.SinLFO;
 import heronarts.lx.parameter.BooleanParameter;
 import heronarts.lx.parameter.CompoundParameter;
+import heronarts.lx.parameter.DiscreteParameter;
 import heronarts.lx.parameter.LXParameter;
 import heronarts.lx.pattern.LXPattern;
 import heronarts.lx.transform.LXVector;
@@ -20,6 +21,8 @@ public class Ripple extends LXPattern {
   CompoundParameter hueParameter = new CompoundParameter("Hue", 0.5).setDescription("Sets hue of wave");
   CompoundParameter speedParameter = new CompoundParameter("Speed", 1, -5, 5).setDescription("Sets speed of wave movement").setPolarity(LXParameter.Polarity.BIPOLAR);
   CompoundParameter wavelengthParameter = new CompoundParameter("Wavelength", 0.5, 0.005, 0.25).setDescription("Sets wavelength between peaks");
+//  CompoundParameter wavelengthParameter = new CompoundParameter("Wavelength", 0.01, 0.0001, 0.01).setDescription("Sets wavelength between peaks");
+  BooleanParameter tinyWavelengthParameter = new BooleanParameter("TinyWavelength", false).setDescription("Makes the wavelength tiny");
   CompoundParameter sizeParameter = new CompoundParameter("Size", -0.1, -3, 1).setDescription("Changes size of wave by applying exponential scaling").setPolarity(LXParameter.Polarity.BIPOLAR);
   CompoundParameter shapeParameter = new CompoundParameter("Shape", -0.5, -2, 1).setDescription("Applies shaping to the waveshape").setPolarity(LXParameter.Polarity.BIPOLAR);
   BooleanParameter grayscaleParameter = new BooleanParameter("Gray", false).setDescription("Sets output to grayscale");
@@ -36,6 +39,7 @@ public class Ripple extends LXPattern {
 
     addParameter(speedParameter.getLabel().toLowerCase(), speedParameter);
     addParameter(wavelengthParameter.getLabel().toLowerCase(), wavelengthParameter);
+    addParameter(tinyWavelengthParameter.getLabel().toLowerCase(), tinyWavelengthParameter);
     addParameter(sizeParameter.getLabel().toLowerCase(), sizeParameter);
     addParameter(shapeParameter.getLabel().toLowerCase(), shapeParameter);
     addParameter(hueParameter.getLabel().toLowerCase(), hueParameter);
@@ -58,7 +62,12 @@ public class Ripple extends LXPattern {
     t = t % TAU;
     updateCenter();
 
-    float freq = 1.f / (wavelengthParameter.getValuef() * maxModelRange);
+    boolean tiny = tinyWavelengthParameter.getValueb();
+    float wavelength = wavelengthParameter.getValuef();
+    if (tiny) {
+      wavelength = wavelength / 50;
+    }
+    float freq = 1.f / (wavelength * maxModelRange);
     float offset;
 
     float exp = sizeParameter.getValuef() * -1; // flip sign to make knob more intuitive: higher value = larger peak
